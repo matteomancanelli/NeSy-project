e213554e49cf991d8ff1222832ba6fb69b3ed4fd
Francesco Petri <francesco.petri@uniroma1.it>
2024-07-01 14:24:58 +0200

    Add more alphabets

--- real_data/generate_alphabet.py
+++ real_data/generate_alphabet.py
@@ -2,16 +2,57 @@ from pathlib import Path
 import pm4py
 import json
 
-def generate_alphabet():
+def generate_alphabet_concept():
     symbols = set()
 
     for filename in Path(__file__).parent.iterdir():
         if filename.suffix == ".xes":
             log = pm4py.read_xes(str(filename))
-            symbols.update(set(log["concept:name"].unique()))
+            symbols.update(log["concept:name"].unique())
 
-    with open(Path(__file__).parent / "ALPHABET.LIST", "w") as f:
-        json.dump(sorted(list(symbols)), f)
+    with open(Path(__file__).parent / "ALPHABET_c.LIST", "w") as f:
+        json.dump(sorted(symbols), f)
+    
+    return len(symbols)
+
+def generate_alphabet_transition():
+    symbols = set()
+
+    for filename in Path(__file__).parent.iterdir():
+        if filename.suffix == ".xes":
+            log = pm4py.read_xes(str(filename))
+            if "lifecycle:transition" in log.columns:
+                symbols.update(log["lifecycle:transition"].unique())
+
+    with open(Path(__file__).parent / "ALPHABET_t.LIST", "w") as f:
+        json.dump(sorted(symbols), f)
+    
+    return len(symbols)
+
+def generate_alphabet_concept_transition():
+    big_symbols = set()
+    small_symbols = set()
+
+    for filename in Path(__file__).parent.iterdir():
+        if filename.suffix == ".xes":
+            log = pm4py.read_xes(str(filename))
+            if "lifecycle:transition" in log.columns:
+                log = log[["concept:name", "lifecycle:transition"]]
+                big_symbols.update(tuple(row) for row in log.values.tolist())
+            else:
+                small_symbols.update(log["concept:name"].unique())
+
+    symbols = set(map(lambda tup: "_".join(tup).replace(" ", "_").lower(), big_symbols))
+    symbols.update(set(map(lambda s: s.lower(), small_symbols)))
+
+    with open(Path(__file__).parent / "ALPHABET_ct.LIST", "w") as f:
+        json.dump(sorted(symbols), f)
+
+    return len(symbols)
 
 if __name__ == "__main__":
-    generate_alphabet()
+    lc = generate_alphabet_concept()
+    lt = generate_alphabet_transition()
+    lct = generate_alphabet_concept_transition()
+
+    print(lc, lt, lct)
